d138babee020a01e22e9a5ddf292630ec77ebf62
Yuriy Mykytyuk <myk.yura@gmail.com>
2011-06-15 14:50:10 +0300

    add splash screen on relaunch

--- Resources/app.js
+++ Resources/app.js
@@ -1,7 +1,7 @@
 Ti.include('controls/global_custom_message.js');
 Ti.include('controls/global_load_indicator.js');
 
-var appBase = Ti.UI.createWindow({ backgroundColor:'#ffffff', });
+var appBase = Ti.UI.createWindow({ backgroundColor:'#ffffff', backgroundImage: 'Default.png' });
 var navGroup = Ti.UI.iPhone.createNavigationGroup({	 });	
 var winHome = Ti.UI.createWindow({
 	backgroundColor:'#ffffff',
@@ -14,4 +14,25 @@ var winHome = Ti.UI.createWindow({
 });
 navGroup.window = winHome;
 appBase.add(navGroup);
+
+var splash = Titanium.UI.createWindow({
+    top: 0,
+    left: 0,
+    width: 320,
+    height: 460,
+    //modal: true,
+    backgroundImage: 'Default.png'
+});
+Ti.App.addEventListener('pause', function(e) {
+	//Ti.API.info('pause event fired: ' + Ti.App.getArguments());
+	//navGroup.open(splash);
+	splash.open();
+});
+
+Ti.App.addEventListener('resume', function(e) {
+	//Ti.API.info('Resume event fired: ' + Ti.App.getArguments());
+	//splash.open();
+    setTimeout(function() { splash.close(); /*navGroup.close(splash);*/ }, 1000);
+});
+
 appBase.open();
